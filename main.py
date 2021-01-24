bff1d45fde91d47d9179295f9311268a9ed28c35
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2021-01-25 03:20:29 +0600

    update 2

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-
 import spacy
-nlp=spacy.load("en")
+
+nlp = spacy.load("en")
+
 
 def check_sent(sent):
-    aux =["am" ,"is" ,"are" ,"was" ,"were" ,"have" ,"has" ,"had" ,"have been" ,"has been" ,"had been"] k=0
-    if sent[0].pos_=="A DV ":
-        k=1
-    if (sent[k].tag_=="V B" ):
+    aux = ["am", "is", "are", "was", "were", "have", "has", "had", "have been", "has been", "had been"]
+    k = 0
+    if sent[0].pos_ == "ADV":
+        k = 1
+    if sent[k].tag_ == "VB":
         return "This is a command sentence"
-    elif (sent[k].tag_=="V BP " and str(sent[k]).lower() not in aux):
-        if len(sent)>k+1 :
-            if sent[k+1]. dep_!="n su bj":
+    elif sent[k].tag_ == "VBP" and str(sent[k]).lower() not in aux:
+        if len(sent) > k + 1:
+            if sent[k + 1].dep_ != "n su bj":
                 return "This is a command sentence"
             else:
                 return "other sentence"
@@ -19,4 +21,6 @@ def check_sent(sent):
     else:
         return "other sentence"
 
+
+s = nlp("dance like a clown")
 print(check_sent(s))
