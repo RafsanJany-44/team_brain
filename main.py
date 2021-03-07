33078713bffbc1e473036393e808a995e822ea04
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2021-03-07 12:32:43 +0600

    update 6

--- main.py
+++ main.py
@@ -3,40 +3,13 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def getResponse():
-    if request.method == 'POST':
-        text = request.form['text']
-        user = request.form['user']
-        if user == '':
-            user = "unknown"
-        # todo some magic
-        response = text # function call
-
-        if response == 'command':
-            print("call micro")
-
-            # todo API call user and text
-
-            return "hi  " + user + " : "
-        elif response == "quary":
-            print("call another mocro")
-
-            # todo API call user and text
-
-            # API type = POST
-            # from body(query,username)
-
-            return "hi  " + user + " : "
-        else:
-            return "My apology, I don't understand!"
-    elif request.method == 'GET':
-
-        return "Hi"
+    return "is hi thare hello"
 
 
-@app.route('/getRec', methods=['GET'])
-def getRecommandation():
-    return "some recommadation"
+
+if __name__=="__main__":
+    app.run()
 
 #lets make contribution
